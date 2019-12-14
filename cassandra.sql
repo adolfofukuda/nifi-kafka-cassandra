4dcfd8af5e8a09eeb95af0ad17ca13a2fa46d808
adolfofukuda <adolfo.fukuda@gmail.com>
2019-12-14 13:14:42 -0300

    carga

---
+++ cassandra.sql
@@ -0,0 +1,5 @@
+-- docker exec -it <container-id> /bin/bash
+--cqlsh --username cassandra --password cassandra 
+CREATE KEYSPACE demo WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 3};
+user demo;
+create table pessoa (cpf varchar, nome varchar, nasc int, cep varchar, PRIMARY KEY (cpf));
