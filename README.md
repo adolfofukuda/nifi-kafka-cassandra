ebdef4e78bbbc68d62e0523a6c1e7f86426a6b77
Adolfo Noriyuki Fukuda <rperformance.adolfo@saraiva.com.br>
2020-05-30 22:57:40 -0300

    correcao na porta do kafka connect

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+copy kafka-connect-cassandra tar content into connectors folder
+create database table using cassandra.sql
 java -jar kafka-connect-cli-0.5-all.jar create cassandra-sink-pessoa < cassandra-sink-pessoa.properties
+no nifi fazer o upload do template nifi-kafka-cassandra.xml e ativar as configuracoes, clicando com o botao direito do mouse -> configure
+rodar o nifi e copiar o output.txt em /tmp
--- cassandra.sql
+++ cassandra.sql
@@ -1,5 +1,4 @@
 -- docker exec -it <container-id> /bin/bash
 --cqlsh --username cassandra --password cassandra 
 CREATE KEYSPACE demo WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 3};
-user demo;
-create table pessoa (cpf varchar, nome varchar, nasc bigint, cep varchar, seletor varchar, PRIMARY KEY (cpf));
+create table demo.pessoa (cpf varchar, nome varchar, nasc bigint, cep varchar, seletor varchar, PRIMARY KEY (cpf));
